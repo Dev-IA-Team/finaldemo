321bb56608995606cb5b89b9b5e93b306ec1c372
root <amziane.hamedi@devoteam.com>
2017-07-21 15:53:53 +0200

    After IBM

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
  */
 
 'use strict';
-
+var http = require("https");
 var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
 var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
@@ -68,29 +68,56 @@ app.post('/api/message', function(req, res) {
  * @return {Object}          The response with the updated message
  */
 function updateMessage(input, response) {
+console.log("Test");
   var responseText = null;
   if (!response.output) {
     response.output = {};
   } else {
-    return response;
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
-    }
-  }
-  response.output.text = responseText;
-  return response;
+
+if (response.entities.length > 0){
+console.log("test-moi");
+	console.log(response.entities[0].entity);
 }
 
+if (response.entities.length > 0 && response.entities[0].entity === 'Oui-Non' && response.context.incident_var === true){
+
+
+var options = {
+  "method": "POST",
+  "hostname": "dev34425.service-now.com",
+  "port": null,
+  "path": "/api/now/table/u_bot_table",
+  "headers": {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Basic U2FkbWluOlNhZG1pbg==",
+    "cache-control": "no-cache",
+    "postman-token": "07bff158-4938-d031-85c2-6b11103ea0f4"
+  }
+}; 
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
+
+	 response.output.text = "Incident créé";
+  return response;
+	
+}
+	
+}
+
+  return response;
+}
 module.exports = app;
